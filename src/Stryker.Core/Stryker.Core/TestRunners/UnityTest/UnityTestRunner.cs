5534461adf700b37075754c79e3f7376a77a6330
Tobias Mattsson <tmatts@kth.se>
2024-03-20 13:40:21 +0100

    Implemented test discovery for Unity tests

--- src/Stryker.Core/Stryker.Core/TestRunners/UnityTest/UnityTestRunner.cs
+++ src/Stryker.Core/Stryker.Core/TestRunners/UnityTest/UnityTestRunner.cs
@@ -15,6 +15,7 @@ using Stryker.Core.Options;
 using Stryker.Core.ProjectComponents.TestProjects;
 using Stryker.DataCollector;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace Stryker.Core.TestRunners.UnityTest
 {
@@ -28,7 +29,19 @@ namespace Stryker.Core.TestRunners.UnityTest
         }
 
         public IEnumerable<CoverageRunResult> CaptureCoverage(IProjectAndTests project) => throw new NotImplementedException();
-        public bool DiscoverTests(string assembly) => throw new NotImplementedException();
+        public bool DiscoverTests(string assembly)
+        {
+            var testDLL = Assembly.LoadFile(assembly);
+            foreach (var type in testDLL.GetTypes())
+            {
+                foreach (var method in type.GetMethods())
+                {
+                    if (method.GetCustomAttributes(typeof(NUnit.Framework.TestAttribute), true).Length > 0) return true;
+                }
+            }
+
+            return false;
+        }
         public void Dispose() => throw new NotImplementedException();
         public TestSet GetTests(IProjectAndTests project) => throw new NotImplementedException();
         public TestRunResult InitialTest(IProjectAndTests project) => throw new NotImplementedException();
