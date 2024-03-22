915ce1e4d64d59be910ee1af137d626c98b3b342
Tobias Mattsson <tmatts@kth.se>
2024-03-22 18:19:36 +0100

    Test set creation added to UnityTestRunner

--- src/Stryker.Core/Stryker.Core/TestRunners/UnityTest/UnityTestRunner.cs
+++ src/Stryker.Core/Stryker.Core/TestRunners/UnityTest/UnityTestRunner.cs
@@ -22,28 +22,38 @@ namespace Stryker.Core.TestRunners.UnityTest
     public sealed class UnityTestRunner : ITestRunner
     {
         private Process _testRun;
+        private TestSet _testSet;
 
         public UnityTestRunner(StrykerOptions options, IFileSystem fileSystem = null)
         {
             _testRun = new Process();
+            _testSet = new TestSet();
         }
 
         public IEnumerable<CoverageRunResult> CaptureCoverage(IProjectAndTests project) => throw new NotImplementedException();
         public bool DiscoverTests(string assembly)
         {
-            var testDLL = Assembly.LoadFile(assembly);
+            var testDLL = Assembly.LoadFrom(assembly);
             foreach (var type in testDLL.GetTypes())
             {
                 foreach (var method in type.GetMethods())
                 {
-                    if (method.GetCustomAttributes(typeof(NUnit.Framework.TestAttribute), true).Length > 0) return true;
+                    if (method.GetCustomAttributes(typeof(NUnit.Framework.TestAttribute), true).Length > 0)
+                    {
+                        // NOTE: Currently, this does not take the Unity test tag into account, meaning it
+                        // can only identify NUnit test cases. Furthermore, no testFilePath can be provided
+                        // since it doesn't seem possible to get the source file of a type or method directly
+                        // from the assembly. This will need to be improved later
+                        var testDesc = new TestDescription(Guid.NewGuid(), method.Name, string.Empty);
+                        _testSet.RegisterTest(testDesc);
+                    }
                 }
             }
 
             return false;
         }
         public void Dispose() => throw new NotImplementedException();
-        public TestSet GetTests(IProjectAndTests project) => throw new NotImplementedException();
+        public TestSet GetTests(IProjectAndTests project) => _testSet;
         public TestRunResult InitialTest(IProjectAndTests project) => throw new NotImplementedException();
         public TestRunResult TestMultipleMutants(IProjectAndTests project, ITimeoutValueCalculator timeoutCalc, IReadOnlyList<Mutant> mutants, TestUpdateHandler update) => throw new NotImplementedException();
     }
